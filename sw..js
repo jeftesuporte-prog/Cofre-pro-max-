65845f13b7637f87da6a3e5a4c0b22785410e61e
jeftesuporte-prog <jeftesuporte@gmail.com>
2026-06-25 16:21:29 -0300

    Refactor service worker caching logic

--- sw..js
+++ sw..js
@@ -4,29 +4,17 @@ const urlsToCache = [
   './index.html',
   './manifest.json',
   './icon-192.png',
-  './icon-512.png',
-  './screenshot1.png',
-  './screenshot2.png'
+  './icon-512.png'
 ];
 
-// Instala o Service Worker e salva os arquivos no cache
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Cache aberto');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Busca arquivos do cache quando tiver offline
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Retorna do cache se tiver, senão busca na rede
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
